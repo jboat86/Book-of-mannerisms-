b1134dc3a78de387b31e9e1a2f0a7a72cf4942ff
Jennifer Boateng <jennifer>
2026-08-24 16:50:15 +0100

    produced data for countries and their mannerisms

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
+<<<<<<< HEAD
 # Book-of-mannerisms-
 handy travel guide for understanding mannerisms used by locals in countries they visit 
+=======
+cmd + Cntrl + space = get emojis 
+>>>>>>> b2da79d (produced data for countries and their mannerisms)
---
+++ data.js
@@ -0,0 +1,227 @@
+const TRAFFICLIGHTS = [{
+    
+    color: "green",
+    icon: "🟢",
+    label: "safe to use", 
+
+    color: "amber",
+    icon: "🟡",
+    label: "could use but exercise caution, depends on situation", 
+
+    color: "red",
+    icon: "🔴",
+    label: " may offend if used, avoid!", 
+}]
+
+
+
+
+
+const COUNTRIES = [{
+    code: "gh", 
+    name: "Ghana",
+    flag: "🇬🇭",
+    mannerisms: [{
+        id: "gh-1",
+        type:"sound",
+        icon: "🟢",
+        title: "Eii",
+        // image: "",
+        meaning: "shock, surprise, disbelief, I'm shocked, OMG!, What!?",
+        context: "Casual conversrations with friends, family or peers",
+        usage_tip: "Totally fine to use. Shows you are engaged. Tone matters- higher pitch = shock",
+        equivalent: "🇳🇬 Ah-Ah/Ehn (exaggerated)",
+        
+        id: "gh-2",
+        type:"sound",
+        icon: "🟢",
+        title: "Hmmm",
+         // image: "",
+        meaning: "worry",
+        context: "Friend delivers bad news",
+        usage_tip: "use when you someone confides in you about an issue they have",
+        equivalent: "🇳🇬 Chai",
+     
+
+        id: "gh-3",
+        type:"sound",
+        icon: "🔴",
+        title: "tsk-tsk",
+         // image: "",
+        meaning: "disagreement, refusal, end of discussion",
+        context: "Friend says something you're not happy with",
+         //   usage_tip: "",
+        equivalent: "🇳🇬 ehn-ehn (repeated rapidly)",
+       
+
+    
+}],
+
+   code: "ng", 
+    name: "Nigeria",
+    flag: "🇳🇬",
+    mannerisms: [{
+        id: "ng-1",
+        type:"sound",
+        icon: "🟢",
+        title: "Ah-Ah",
+         // image: "",
+        meaning: "shock, surprise, disbelief",
+        context: "Friend tells crazy story",
+         //   usage_tip: "",
+        equivalent: "🇧🇴 Eii",
+        
+        id: "ng-2",
+        type:"sound",
+        icon: "🟡",
+        title: "Chai",
+         // image: "",
+        meaning: " worry",
+        context: "Friend delivers bad news",
+         //   usage_tip: "",
+        equivalent: "🇧🇴 Hmmm ",
+
+
+        id: "ng-3",
+        type:"sound",
+        icon: "🟡",
+        title: "ehn!",
+         // image: "",
+        meaning: "shock, surprise",
+        context: "Friend says they are getting engaged",
+         //   usage_tip: "",
+        equivalent: "🇧🇴 Eii",
+
+        id: "ng-4",
+        type:"sound",
+        icon: "🔴",
+        title: "ehn-ehn",
+         // image: "",
+        meaning: "disagreement, refusal, end of discussion",
+        context: "Friend says something you're not happy with",
+         //   usage_tip: "",
+        equivalent: "🇧🇴 tsk-tsk",
+
+        id: "ng-5",
+        type:"sound",
+        icon: "🔴",
+        title: "chai",
+         // image: "",
+        meaning: "worry",
+        context: "heared some bad news",
+         //   usage_tip: "",
+        equivalent: "🇧🇴 tsk-tsk",
+        
+    
+}],
+
+   code: "jp", 
+    name: "Japan",
+    flag: "🇯🇵",
+    mannerisms: [{
+        id: "jp-1",
+        type:"sound",
+        icon: "🟢",
+        title: "Hai",
+         // image: "",
+        meaning: "Yes, I'm listening",
+        description: "In Japan: Say 'hai' to show you are listening and understand what someone is saying. It is considered rude to not acknowledge what someone is saying.",   
+        context: "listening, show that you understand what someone is saying",
+         //   usage_tip: "",
+        equivalent: "🇧🇴 'Aah'!/🇺🇸 'Mhm' ",
+        
+        id: "jp-2",
+        type:"sound",
+        icon: "🟡",
+        title: "E?",
+         // image: "",
+        meaning: " Huh?, Really?",
+        context: "surprised but use soft pitched voice to show you are not being rude",
+        usage_tip: "Do not use a high pitched voice, it is considered rude",
+        equivalent: "🇺🇸 'Mhm'/🇧🇴 'Aah (medium pitch)'",
+
+        id: "jp-3",
+        type:"sound",
+        icon: "🔴",
+        title: "Nuh-uh",
+         // image: "",
+        meaning: "direct no",
+        context: "disagreeing with someone",
+        usage_tip: "chotto (That is a bit...) instead",
+        equivalent: "🇺🇸 'Nah'/🇧🇴  abrupt'Aah (high pitch)'",
+      
+    
+}],
+
+    code: "jm", 
+    name: "Jamaica",
+    flag: "🇯🇲",
+    mannerisms: [{
+        id: "jm-1",
+        type:"hand gesture",
+        icon: "🟢",
+        title: "Hand on chest",
+         // image: "",
+        meaning: "one love, Hello ",
+        description: "warm greeting",   
+        context: "meeting someone for the first time",
+         //   usage_tip: "",
+      
+
+        id: "jm-2",
+        type: "head movement",
+        icon: "🟢", 
+        title: "wah gwan",
+         // image: "",
+        meaning: "Hello, What's up",
+        context: "warm greeting among friends",
+         //   usage_tip: "",
+       
+        
+        id: "jm-3",
+        type:"sound",
+        icon: "🟡",
+        title: "E?",
+         // image: "",
+        meaning: " Huh?, Really?",
+        context: "surprised but use soft pitched voice to show you are not being rude",
+        avoid: "Do not use a high pitched voice, it is considered rude",
+         //   usage_tip: "",
+        equivalent: "🇺🇸 'Mhm'/🇧🇴 'Aah (medium pitch)'",
+
+        id: "jm-4",
+        type: "sound",
+        icon: "🟡/🔴",
+        title: "Mchew/Teeth sucking sound",
+         // image: "",
+        meaning: "annoyance, disagreement and expressing tiredness/exhaustion",
+        context: "Non-verbal expression. Intensity changes meaning from serious to playful",
+         //   usage_tip: "",
+
+     
+        id: "jm-5",
+        type:"lip gesture",
+        icon: "🟡",
+        title: "Pointing with lips",
+         // image: "",
+        context: "Directing attention without using hands",
+        meaning: "Subtle way to indicate direction, object, or person", 
+         //   usage_tip: "",   
+      
+
+        id: "jm-6",
+        type:"sound",
+        icon: "🔴",
+        title: "Nuh-sah",
+         // image: "",
+        meaning: " direct No",
+        context: "disagreeing with someone",
+         //   usage_tip: "",
+        equivalent: "🇺🇸 'Nah'/🇧🇴  abrupt'Aah (high pitch)'",
+      
+    
+}],
+
+
+
+}]
